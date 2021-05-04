0b19ccc293a8575ed55b625ee9c2cb36ccf36847
Cagatay Kaydir <cagataykaydir@gmail.com>
2021-05-05 00:51:23 +0300

    .;

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: `${process.env.NEXT_PUBLIC_PRODUCTION ? '/graphql' : 'localhost:5000/graphql'}`,
+  uri: `http://${process.env.NEXT_PUBLIC_PRODUCTION ? 'backend' : 'localhost'}:5000/graphql`,
+  // uri: `${process.env.NEXT_PUBLIC_PRODUCTION ? '/graphql' : 'localhost:5000/graphql'}`,
   // uri: 'http://127.0.0.1:5000/graphql',
   cache: new InMemoryCache(),
 });
