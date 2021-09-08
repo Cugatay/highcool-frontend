e4f90d8fc1d7af65a7d28bc284f0c0cc4ea653ad
Cagatay Kaydir <cagataykaydir@gmail.com>
2021-09-08 22:27:08 +0300

    url change

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: `${process.env.NEXT_PUBLIC_PRODUCTION ? 'https://api.prisold.com/graphql' : 'http://localhost:5000/graphql'}`,
+  uri: `${process.env.NEXT_PUBLIC_PRODUCTION ? 'https://prisold.com/api/graphql' : 'http://localhost:5000/graphql'}`,
   // uri: 'http://192.168.1.25:5000/graphql',
   cache: new InMemoryCache(),
 });
